0240ac93f0bf173b6da817327329216af173b060
chx9 <cheng.huan@icloud.com>
2024-12-31 16:13:31 +0000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE // Must: define SPDLOG_ACTIVE_LEVEL before `#include "spdlog/spdlog.h"`
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_sinks.h"
+
+void source_info_example()
+{
+    auto console = spdlog::stdout_logger_mt("console");
+    spdlog::set_default_logger(console);
+    spdlog::set_pattern("[source %s:%#] [function %!] %v");
+
+    SPDLOG_LOGGER_INFO(console, "log with source info"); // Console: "[source example.cpp] [function source_info_example] [line 10] log with source info"
+    SPDLOG_INFO("global log with source info"); // Console: "[source example.cpp] [function source_info_example] [line 11] global logger with source info"
+
+    console->info("source info is not printed"); // Console: "[source ] [function ] [line ] source info is not printed"
+}
 int main(){
-    std::cout<<"pluster"<<std::endl;  
+    source_info_example();
+
     return 0;
 }
